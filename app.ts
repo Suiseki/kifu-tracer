f6b6dc7135af4bf914d53e16b5f03414dae75e27
Bartosz Kamiński <bartosz.kaminski@cognifide.com>
2021-07-13 09:22:42 +0200

    comments for different styles of types

--- app.ts
+++ app.ts
@@ -1,18 +1,20 @@
 // case Function expects objects with numbers
 
-const input = {a: 1, b: 3, c: 3}
+const input = {a: 1, b: 3}
 
-const combinator = (someObj: {a: number, b: number}) => {
+// setting types directly in the function
+const combinator = (someObj: {a: number, b: number}):number => {
     return someObj.a + someObj.b
 }
 
+// using type alias to set types
 type CombinatorImproved = {a: number, b: string}
 
-const combinatorImproved = (someObj:CombinatorImproved) => {
+const combinatorImproved = (someObj:CombinatorImproved): number | string => {
     return someObj.a + someObj.b
 }
 
-console.log('result is: ', combinator(input))
+console.log('Calculation result is: ', combinator(input))
 
 console.log('improved result is: ', combinatorImproved(input))
  // end
@@ -25,6 +27,6 @@ const elms = [1234,3,42]
 
 const result = last([1234,'3',42])
 
-const resultSt = last<string>(['Bill','George','Will'])
+const resultSt = last<string>(['Bill','George','Will', 'Steven'])
 
 console.log('array last el:', result, resultSt)
